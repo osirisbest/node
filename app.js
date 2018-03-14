ec1e31f5a4e75d7686cc519ae24fc4190306c7f9
osirisbest <osirisbest@gmail.com>
2018-03-14 22:59:16 +0200

    fs

---
+++ app.js
@@ -0,0 +1,23 @@
+let http=require('http')
+let fs=require('fs')
+
+function writeNumbers(res){
+ //let i=0
+ for (let i=0;i++;i<20){
+    res.write(i.toString+'\n')
+ }   
+}
+
+http.createServer((req,res)=>{
+    res.writeHead(200,{'content-type':'text-plane'})
+    fs.readFile('test.txt','utf-8',(err,data)=>{
+        if (err) res.write(err.toString())
+            else res.write(data)
+        setTimeout(()=>{res.write('data')
+        res.end()
+    },10000)
+        
+    })
+    //res.end('Hello!')
+}).listen(8888)
+console.log('listen 8888')
