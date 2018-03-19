f2229997c29e5010d8983ea8d7bbed4352ea89d1
osirisbest <osirisbest@gmail.com>
2018-03-19 16:31:13 +0200

    require

--- socketChat/app.js
+++ socketChat/app.js
@@ -1,4 +1,4 @@
-var app = require('http').createServer(handler),
+let app = require('http').createServer(handler),
   io = require('socket.io').listen(app),
   fs = require('fs')
 app.listen(8124)
@@ -13,7 +13,7 @@ function handler (req, res) {
       res.end(data)
     })
 }
-io.sockets.on('connection', function (socket) {
+io.sockets.on('connection', (socket) => {
   socket.on('addme', (username) => {
     socket.username = username
     socket.emit('chat', 'SERVER', 'You have connected')
